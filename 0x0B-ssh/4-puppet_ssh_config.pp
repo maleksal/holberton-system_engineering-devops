1ba6b623f2528e3dd8ed224f3ed0f7854ffb691c
Malek Salem <malek.salem.14@gmail.com>
2020-08-04 17:42:44 +0000

    fix red checks

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # configure ssh connection with puppet
 exec { "configure_ssh":
-	command => 'echo -e "\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/holberton"" >> /etc/ssh/ssh_config',
-	path => ['/bin'],
+	command => 'echo -e "\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+	path => ['/bin',],
 }
