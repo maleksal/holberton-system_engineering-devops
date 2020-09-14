abe169f0e90d25ed9d15b897af0cdc9540b8ec36
Malek Salem <malek.salem.14@gmail.com>
2020-09-14 18:03:15 +0100

    add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+API module
+
+"""
+import requests
+import sys
+
+
+def request_from_api(_id):
+    """
+    Request for a given employee ID,
+    returns information about his/her TODO list progress.
+
+    """
+    # get employee name
+    employee_name = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + _id).json()['name']
+
+    # get employee info
+    info = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + _id).json()
+    completed_t = 0
+    all_t = 0
+    titels = []
+    for el in info:
+        if el.get("completed"):
+            titels.append(el.get("title"))
+            completed_t += 1
+    all_t += 1
+    print(
+        'Employee {} is done with tasks({}/{}):'.format(employee_name,
+                                                        completed_t,
+                                                        all_t))
+    [print(i) for i in titels]
+
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    request_from_api(employee_id)
