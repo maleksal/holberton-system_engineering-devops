b28fd7780912590a8858531a38ee4e56613a5b6f
Malek Salem <malek.salem.14@gmail.com>
2020-09-17 20:25:46 +0100

    fix bug

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -16,19 +16,22 @@ def recurse(subreddit, hot_list=[], end=None):
         "User-Agent": "Ubuntu:playing with API (by /u/Cyber)"}
     # get sub-reddit info
     request_info = requests.get(
-        main_url + '/r/{}/hot.json'.format(subreddit),
+        main_url + '/r/{}/hot.json?after={}'.format(subreddit, end),
         headers=headers,
         allow_redirects=False,
         )
-    print(len(hot_list))
     if request_info.status_code == 404:
         return None
     hottest = request_info.json().get("data").get("children")
     for i in hottest:
         hot_list.append(i.get("data").get("title"))
         _end = i.get('data').get('after')
-        if not _end:
-            return hot_list
-        return recurse(subreddit, hot_list, _end)
+
+    heckya = request_info.json().get("data").get("after")
+    if not heckya:
+        return hot_list
+    return recurse(subreddit, hot_list, heckya)
+
+
 if __name__ == "__main__":
     number_of_subscribers()
