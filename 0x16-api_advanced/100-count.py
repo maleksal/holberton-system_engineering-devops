1008843509aab09d9ac3ed37f420b589c0c0457e
Malek Salem <malek.salem.14@gmail.com>
2020-09-17 22:02:25 +0100

    fix bugssss

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
 Advanced API module
-
 """
-from collections import OrderedDict
 import requests
 
 
@@ -32,35 +30,34 @@ def count_words(subreddit, word_list, dictionary={}, end=None, init=False):
         allow_redirects=False,
         )
     if request_info.status_code == 404:
-        return print()
+        return None
     try:
         hottest = request_info.json().get("data").get("children")
-
-        for i in hottest:
-            for e in word_list:
-                if not init:
-                    dictionary[e] = counter(e, i.get("data").get("title"))
-                else:
-                    len_ = dictionary[e] + counter(e, i.get("data").get("title"))
-                    dictionary[e] = len_
-        init = True
-        # check for exit
-        heckya = request_info.json().get("data").get("after")
-        if not heckya:
-            sorted_dict = OrderedDict(sorted(
-                                        dictionary.items(),
-                                        key=lambda x: x[1],
-                                        reverse=True))
-            if len(sorted_dict) != 0:
-                for key, val in sorted_dict.items():
-                    if val != 0:
-                        print("{}: {}".format(key, val))
+    except:
+        return
+    for i in hottest:
+        for e in word_list:
+            if not init:
+                dictionary[e] = counter(e, i.get("data").get("title"))
             else:
-                print()
-            return
-        return count_words(subreddit, word_list, dictionary, heckya, init)
-    except Exception:
-        pass
+                len_ = dictionary[e] + counter(e, i.get("data").get("title"))
+                dictionary[e] = len_
+    init = True
+    # check for exit
+    heckya = request_info.json().get("data").get("after")
+    if not heckya:
+        if len(set(list(dictionary.values()))) <= 1:
+            sorted_list = sorted(list(dictionary.items()))
+        else:
+            sorted_list = sorted(
+                            dictionary.items(),
+                            key=lambda x: x[1],
+                            reverse=True)
+        for key, val in sorted_list:
+            if val != 0:
+                print("{}: {}".format(key, val))
+        return
+    return count_words(subreddit, word_list, dictionary, heckya, init)
 
 
 if __name__ == "__main__":
