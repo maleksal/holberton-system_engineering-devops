f4a5d6336e1b2534e0639987d10972ab3c7c2667
maleksal <malek.salem.14@gmail.com>
2020-10-08 16:11:00 +0100

    fix bug in service restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Fix nginx failed requests
 
 exec { 'fix':
-  command => 'sed -i \'s/^ULIMIT=.*/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+  command => 'sed -i \'s/^ULI.*=.*/ULIMIT="-n 4096"/g\' /etc/default/nginx',
   path    => '/bin/',
 }
 
-exec { 'restart nginx server':
-  command => 'servide nginx restart',
+-> exec { 'restart nginx server':
+  command => 'nginx restart',
   path    => '/etc/init.d/',
 }
