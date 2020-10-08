f28260f1ef46a5d7e0eeed4376ea648950ca243c
maleksal <malek.salem.14@gmail.com>
2020-10-08 16:06:57 +0100

    add task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Fix nginx failed requests
+
+exec { 'fix':
+  command => 'sed -i \'s/^ULI.*=.*/ULIMIT="-n 4096"/g\' /etc/default/nginx',
+  path    => '/bin/',
+}
+
+exec { 'restart nginx server':
+  command => 'servide nginx restart',
+  path    => '/etc/init.d/',
+}
