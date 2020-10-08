82f01fe1e82b90e0ec30185091867ae6dc9de6d2
maleksal <malek.salem.14@gmail.com>
2020-10-08 16:35:34 +0100

    fix red checks

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
-# puser permission
+# user permission
 
-exec { 'hard nofile limit':
-  command => "sed -i -e 's/^holberton hard .*/holberton hard nofile 64000/g' -e 's/^holberton soft .*/holberton soft nofile 64000/g' /etc/security/limits.conf",
+exec { 'hard && soft nofile limit':
+  command => "sed -i -e 's/^holberton hard .*/holberton hard nofile 64000/g'\
+   -e 's/^holberton soft .*/holberton soft nofile 64000/g' /etc/security/limits.conf",
   path    => '/bin/',
 }
